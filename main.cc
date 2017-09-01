28c52d4786417fec83a48fabf57eaa6ef419a3ac
DavidMartinez14 <31513500+DavidMartinez14@users.noreply.github.com>
2017-09-01 11:10:03 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,5 +1,4 @@
 max-width:100%;
-body {
-    background: url<img src="2jtQ1r.png"> no-repeat center center;
-    background-size: cover; 
-}
+body {background: url<img src=("2jtQ1r.png") no-repeat center center;
+    background-size: cover
+    Background-color:#000080
